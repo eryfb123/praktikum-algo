98a21d2936995476ea9c35fe05644e6b9091ab49
eryfb123 <113966566+eryfb123@users.noreply.github.com>
2022-09-22 18:21:43 +0700

    Create ProgramDataAlamat.py

---
+++ ProgramDataAlamat.py
@@ -0,0 +1,7 @@
+# Program untuk menampilkan data alamat inputan user
+
+# Meminta inputan alamat dari user
+address = input("Masukkan alamat: ")
+
+# Menampilkan hasil inputan alamat
+print(f"Alamat: {address}")

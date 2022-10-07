14ba793adf311dc645452cd4287889860e4e3541
eryfb123 <113966566+eryfb123@users.noreply.github.com>
2022-10-07 15:17:25 +0700

    Add files via upload

---
+++ programperulanganfor.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  7 14:12:34 2022
+
+@author: Ery Febrian
+"""
+#program perulangan for
+
+n = int(input("masukan jumlah maksimal ="))
+for a in range(n,0,-1):
+    for b in range(a):
+        print(a, end="")
+    print("\r")
+print("065002200011,Ery Febrian")
+
+
+
